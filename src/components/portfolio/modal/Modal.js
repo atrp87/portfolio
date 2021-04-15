98ed934876228fa423a6b6e444d91ef28c727594
Andrew Peattie <drewpeattie@hotmail.com>
2021-04-15 13:21:49 +0100

    model state

--- src/components/portfolio/modal/Modal.js
+++ src/components/portfolio/modal/Modal.js
@@ -1,70 +1,22 @@
-import { motion, AnimatePresence } from 'framer-motion';
-import ModalCSS from './Modal.module.css';
+import ModalCSS from './Modal.module.css'
+import {createPortal} from 'react-dom';
 import React from 'react';
 
-  const backdropVariants = {
-    initial: {opacity: 1},
-    final: {opacity: 0}
+const Modal = ({ isOpened, children, onClose }) => {
+  if (!isOpened) {
+    return null;
   }
 
-  const modalVariant = {
-    initial: {
-      y: '300px',
-      opacity: 0
-    },
-    final: {
-      opacity: 1,
-    }
-  }
-
-const Modal = ({ showModal, setShowModal }) => {
-  return ( 
-    <div className={ModalCSS.modal}>
-        <AnimatePresence exitBeforeEnter>
-        { showModal && (
-          <motion.div className={ModalCSS.backdrop}
-            variants={backdropVariants}
-            animate='initial'
-            initial="final"
-            exit='final'
-          >
-            <motion.div className={ModalCSS.modal}
-              variants={modalVariant}
-              initial='initial'
-              animate='final'
-            >
-              <p>hello</p>
-                <button>Live</button>
-                <button>Code</button>
-            </motion.div>
-          </motion.div>
-        )}
-        </AnimatePresence>
-    </div>
+  return createPortal ( 
+    <div>
+      <div className={ModalCSS.overlay}></div>
+      <div className={ModalCSS.modal}>
+        <span className={ModalCSS.close} onClick={onClose}>X</span>
+        <div className="modal-content">{children}</div>
+      </div>
+    </div>,
+    document.getElementById('modal')
   );
 }
 
 export default Modal;
-
-// div className={ModalCSS.content}>
-//         <div className={ModalCSS.modalLeft}>
-//           <img src="" alt=""/>
-//         </div>
-//         <div className={ModalCSS.modalRight}>
-//           <div className={ModalCSS.modalHeader}>
-//             <p>TITLE</p>
-//           </div>
-//           <div className={ModalCSS.modalBody}>
-//             <ul>
-//               <li>1</li>
-//               <li>2</li>
-//               <li>3</li>
-//             </ul>
-//           </div>
-//           <div className={ModalCSS.modalFooter}>
-//             <button>Live</button>
-//             <button>Code</button>
-//             <button onClick={props.onClose}>Close</button>
-//           </div>
-//         </div>
-//       </div>
--- src/components/portfolio/projects/Projects.js
+++ src/components/portfolio/projects/Projects.js
@@ -4,29 +4,47 @@ import Covid from '../../../assets/covid19.png';
 import ProjectsCSS from './Projects.module.css';
 import Pilton from '../../../assets/pycp.png';
 import React, { useState } from 'react';
-import CovidM from '../modal/CovidM';
+import Modal from '../modal/Modal';
 
 const Projects = () => {
 	const [isModelOpened, setIsModelOpened] = useState(false);
 
+	
+	
 	return (
 		<div className={ProjectsCSS.wrapper}>
 			<div className={ProjectsCSS.items}>
-				<div className={ProjectsCSS.content}>
+				<div onClick={() => setIsModelOpened(true)} className={ProjectsCSS.content}>
 					<img src={myPortfolio} alt="Portfolio Screenshot" />
 					<i className={`${ProjectsCSS.icon} fas fa-search-plus`} />
 				</div>
+				<Modal
+					isOpened={isModelOpened} 
+        onClose={() => setIsModelOpened(false)}>
+					<p>HELLO 2</p>
+				</Modal>
 			</div>
 
-      <div 
-				onClick={() => setIsModelOpened(true)} className={ProjectsCSS.items}>
-					<div className={ProjectsCSS.content}>
+      <div className={ProjectsCSS.items}>
+					<div onClick={() => setIsModelOpened(true)} className={ProjectsCSS.content}>
 					<img src={Covid} alt="COVID-19 Project Screenshot" />
 					<i className={`${ProjectsCSS.icon} fas fa-search-plus`} />
 				</div>
-				<CovidM 
-					isOpened={isModelOpened} 
-					onClose={() => setIsModelOpened(false)} />
+				 <Modal 
+        isOpened={isModelOpened} 
+        onClose={() => setIsModelOpened(false)}>
+          <form className="login-form" onSubmit={() => isModelOpened(false)}>
+            <section>
+              <label htmlFor="">email</label>
+              <input type="email" name="" id=""/>
+            </section>
+            <section>
+              <label htmlFor="">password</label>
+              <input type="password" name="" id=""/>
+            </section>
+            <button>Sign In</button>
+          </form>
+      </Modal>
 			</div>
 
 			<div className={ProjectsCSS.items}>
