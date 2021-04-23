d917e282484cb3077d7987064466bbda8f2f945b
Andrew Peattie <drewpeattie@hotmail.com>
2021-04-23 13:54:49 +0100

    modal style

--- src/components/portfolio/displayProjects/DisplayProjects.js
+++ src/components/portfolio/displayProjects/DisplayProjects.js
@@ -2,40 +2,82 @@ import DisplayProjectsCSS from './DisplayProjects.module.css';
 import myPortfolio from '../../../assets/myPortfolio.png';
 import Budget from '../../../assets/spendTracker.png';
 import Covid from '../../../assets/covid19.png';
+import CovidGIF from '../../../assets/covid.gif';
 import Pilton from '../../../assets/pycp.png';
 import React, { useState} from 'react';
 import Modal from '../modal/Modal';
 
 const DisplayProjects = () => {
-	const [isModalOpened, setIsModalOpened] = useState(false);
-	console.log('isModalOpened', isModalOpened);
+	const [isModalOpened1, setIsModalOpened1] = useState(false);
+	const [isModalOpened2, setIsModalOpened2] = useState(false);
+	// FIND a more efficient way of state use / reusable Modal component.
+	// && Separate component for image displays / modal.
 
   return ( 
     <div className={DisplayProjectsCSS.wrapper}>
-			<div onClick={() => setIsModalOpened(true)} className={DisplayProjectsCSS.items}>
-				<div className={DisplayProjectsCSS.content}>
-					<img src={myPortfolio} alt="Portfolio Screenshot" />
-					<i className={`${DisplayProjectsCSS.icon} fas fa-search-plus`} />
-				</div>
+			<div 
+				onClick={() => setIsModalOpened1(true)} 
+				className={DisplayProjectsCSS.items}>
+					<div className={DisplayProjectsCSS.content}>
+						<img src={myPortfolio} alt="Portfolio Screenshot" />
+						<i className={`${DisplayProjectsCSS.icon} fas fa-search-plus`} />
+					</div>
 			</div>
 			<Modal 
-				isOpened={isModalOpened} 
-				onClose={() => setIsModalOpened(false)}>
-					<section>
-            <label>1</label>
-          </section>
-			</Modal>
+				isOpened={isModalOpened1} 
+				onClose={() => setIsModalOpened1(false)}>
+						<div className='modal-left'>
+							<img src={CovidGIF} alt="Portfolio GIF"/>
+						</div>
+						<div className="modal-right">
+							<h3>My Portfolio</h3>
+								<ul>
+									<li>React.JS</li>
+									<li>JavaScript</li>
+									<li>FRAMER MOTION</li>
+									<li>REACT ROUTER</li>
+									<li>REACT PORTALS</li>
+								</ul>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ipsam, asperiores dolorem impedit neque dolores sint praesentium laudantium? Alias veritatis incidunt maxime a molestiae amet nam exercitationem laborum quod? Debitis dicta assumenda exercitationem maxime a est veniam sequi quasi commodi soluta maiores, officiis similique rerum veritatis ex nemo incidunt consectetur.
+								</p>
+								<button>Live Version</button>
+								<button>Code</button>
+						</div>
+				</Modal>
 
 
 
 
 
       <div className={DisplayProjectsCSS.items}>
-					<div className={DisplayProjectsCSS.content}>
+					<div onClick={() => setIsModalOpened2(true)} className={DisplayProjectsCSS.content}>
 					<img src={Covid} alt="COVID-19 Project Screenshot" />
 					<i className={`${DisplayProjectsCSS.icon} fas fa-search-plus`} />
 				</div>
 			</div>
+
+						<Modal 
+				isOpened={isModalOpened2} 
+				onClose={() => setIsModalOpened2(false)}>
+						<div className={DisplayProjectsCSS.ModalLeft}>
+							<img src={CovidGIF} alt=""/>
+						</div>
+						<div className={DisplayProjectsCSS.ModalRight}>
+							<h3>COVID-19 "Angela"</h3>
+								<ul>
+									<li>HTML & CSS</li>
+									<li>BOOTSTRAP</li>
+									<li>JavaScript</li>
+									<li>Vue.JS</li>
+									<li>MONGODB</li>
+								</ul>
+								<p>
+									INFO ON PROJECT / SCROLL BAR
+								</p>
+						</div>
+				</Modal>
+
 			<div className={DisplayProjectsCSS.items}>
 				<div className={DisplayProjectsCSS.content}>
 					<img src={Budget} alt="Spending Tracker Project Screenshot" />
--- src/components/portfolio/modal/Modal.js
+++ src/components/portfolio/modal/Modal.js
@@ -1,18 +1,23 @@
-import ModalCSS from './Modal.module.css'
-import {createPortal} from 'react-dom';
+import ModalCSS from './Modal.module.css';
+import { createPortal } from 'react-dom';
 import React from 'react';
 
-const Modal = ({ isOpen, children, onClose }) => {
-  if (!isOpen) {
+const Modal = ({ isOpened, children, onClose }) => {
+  if (!isOpened) {
     return null;
   }
 
   return createPortal ( 
     <div>
       <div className={ModalCSS.overlay}></div>
-      <div className={ModalCSS.modal}>
-        <i className={ `${ModalCSS.close} fas fa-times`} onClick={onClose}></i>
-        <div className="modal-content">{children}</div>
+        <div className={ModalCSS.modal}>
+          <i 
+            onClick={onClose}
+            className={`${ModalCSS.close} fas fa-times`}>
+          </i>
+          <div className={`${ModalCSS.content} model-content`}>
+            {children}
+          </div>
       </div>
     </div>,
     document.getElementById('modal')
